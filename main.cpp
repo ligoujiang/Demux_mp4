d398e5b3f99b699acfe057a33bd176085b97afce
ligoujiang <2571373878@qq.com>
2024-04-17 00:03:45 +0800

    2024/4/17 00:03

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ private:
     AVFormatContext* m_inFmtCtx=nullptr;
     //输出文件句柄
     FILE* m_fp_h264=nullptr;
-    FILE* m_fp_acc=nullptr;
+    FILE* m_fp_aac=nullptr;
     AVPacket* m_pkt=nullptr;
     AVFrame* m_freame=nullptr;
 public:
@@ -37,9 +37,13 @@ public:
             fclose(m_fp_h264);
             av_log(NULL,AV_LOG_DEBUG,"m_fp_h264已被析构！\n");
         }
-        if(m_fp_acc){
-            fclose(m_fp_acc);
+        if(m_fp_aac){
+            fclose(m_fp_aac);
             av_log(NULL,AV_LOG_DEBUG,"m_fp_acc已被析构！\n");
+        }
+        if(m_pkt){
+            av_packet_free(&m_pkt);
+            av_log(NULL,AV_LOG_DEBUG,"m_pkt已被析构！\n");
         }
     }
     bool DemuxMP4(){
@@ -59,8 +63,47 @@ public:
         //打印输入文件信息
         av_dump_format(m_inFmtCtx,0,m_inFileName,0);
 
+        //打开输出文件
+        m_fp_h264=fopen(m_outFileName_H264,"wb");
+        if(!m_fp_h264){
+            av_log(NULL,AV_LOG_ERROR,"open_m_fp_h264 failed!\n");
+            return false;
+        }
+        m_fp_aac=fopen(m_outFileName_Aac,"wb");
+        if(!m_fp_aac){
+            av_log(NULL,AV_LOG_ERROR,"open_m_fp_aac failed!\n");
+            return false;
+        }
+        //查找视频流
+        int videoIndex=av_find_best_stream(m_inFmtCtx,AVMEDIA_TYPE_VIDEO,-1,-1,NULL,0); //查找视频流
+        if(videoIndex==-1){
+            av_log(NULL,AV_LOG_ERROR,"find videoIndex failed!\n");
+            return false;
+        }
+        //查找音频流
+        int audioIndex=av_find_best_stream(m_inFmtCtx,AVMEDIA_TYPE_AUDIO,-1,-1,NULL,0); //查找视频流
+        if(audioIndex==-1){
+            av_log(NULL,AV_LOG_ERROR,"find audioIndex failed!\n");
+            return false;
+        }
 
-
+        //发包
+        m_pkt=av_packet_alloc();
+        while(true){
+            ret=av_read_frame(m_inFmtCtx,m_pkt);
+            if(ret<0){
+                av_log(NULL,AV_LOG_ERROR,"av_read_frame failed!\n");
+                break;
+            }
+            if(m_pkt->stream_index==AVMEDIA_TYPE_VIDEO){
+                //处理视频
+                av_packet_unref(m_pkt);
+            }else if(m_pkt->stream_index==AVMEDIA_TYPE_AUDIO){
+                //处理音频
+                av_packet_unref(m_pkt);
+            }
+            av_packet_unref(m_pkt);
+        }
         return true;
     }
 };
